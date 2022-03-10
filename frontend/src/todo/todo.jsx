fff279a5148cd8939f0440f40405ad27cd9696e5
Everson <eversonpereiraofc@gmail.com>
2022-03-10 14:09:20 -0300

    adicionando evento de onChange no input de adicionar tarefa

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -6,18 +6,24 @@ import TodoList from "./todoList";
 export default class Todo extends Component {
     constructor(props){
         super(props)
+        this.state = {description: "", list: ""}
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e){
+        this.setState({...this.state, description: e.target.value})
     }
 
     handleAdd() {
-        console.log(this)
+        console.log(this.state.description)
     }
 
     render(){
         return (
             <div>
                 <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
-                <TodoForm handleAdd={this.handleAdd}></TodoForm>
+                <TodoForm description={this.state.description} handleAdd={this.handleAdd} handleChange={this.handleChange}></TodoForm>
                 <TodoList></TodoList>
             </div>
         )
